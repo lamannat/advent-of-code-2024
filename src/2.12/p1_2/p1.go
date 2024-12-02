349f6d1302fb21ddfbcd3bd3b582e9b25f21d15c
lamannat <tlamanna@fi.uba.ar>
2024-12-02 16:31:38 -0300

    solved day 2 problem 2

--- src/2.12/p1_2/p1.go
+++ src/2.12/p1_2/p1.go
@@ -21,7 +21,10 @@ func P1() {
 func countSafe(records [][]int) int {
 	safeCount := 0
 	for i := 0; i < len(records); i++ {
-		if recordIsSafe(records[i]) {
+		if recordIsSafeBruteForceAllLevels(records[i]) {
+			if !recordIsSafe(records[i]) {
+				fmt.Println("algorithm bug detected:", records[i])
+			}
 			safeCount++
 		}
 	}
@@ -33,8 +36,13 @@ func recordIsSafe(record []int) bool {
 	if unsafeLevel == -1 {
 		return true
 	}
-	fixedRecord := dumpUnsafeLevel(record, unsafeLevel)
-	unsafeLevel = findUnsafeLevel(fixedRecord)
+	fixedRecord1 := dumpUnsafeLevel(record, unsafeLevel)
+	fixedRecord2 := dumpUnsafeLevel(record, unsafeLevel-1)
+	unsafeLevel = findUnsafeLevel(fixedRecord1)
+	if unsafeLevel == -1 {
+		return true
+	}
+	unsafeLevel = findUnsafeLevel(fixedRecord2)
 	if unsafeLevel == -1 {
 		return true
 	}
@@ -71,5 +79,31 @@ func findUnsafeLevel(record []int) int {
 }
 
 func dumpUnsafeLevel(r []int, level int) []int {
+	tmp := make([]int, 0)
+	if level == 0 {
+		tmp = append(tmp, r[1:]...)
+		return tmp
+	}
+	if level == len(r)-1 {
+		tmp = append(tmp, r[:len(r)-1]...)
+		return tmp
+	}
+	tmp = append(tmp, r[:level]...)
+	tmp = append(tmp, r[level+1:]...)
+	return tmp
+}
 
+func recordIsSafeBruteForceAllLevels(record []int) bool {
+	unsafeLevel := findUnsafeLevel(record)
+	if unsafeLevel == -1 {
+		return true
+	}
+	for i := 0; i < len(record); i++ {
+		fixedRecord := dumpUnsafeLevel(record, i)
+		unsafeLevel = findUnsafeLevel(fixedRecord)
+		if unsafeLevel == -1 {
+			return true
+		}
+	}
+	return false
 }
